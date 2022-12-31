1063d61df298f6458badce0cbd0cc99b6a3c8a23
@anhnv99 <vietanhhappy99@gmail.com>
2022-12-31 15:00:38 +0700

    update

--- src/layouts/System/Page/ManageUser/ManageUser.js
+++ src/layouts/System/Page/ManageUser/ManageUser.js
@@ -2,16 +2,23 @@ import './ManageUser.scss';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteAccount, getAllAccount } from '~/redux/apiRequest';
-import { DeleteOutlined, EyeTwoTone } from '@ant-design/icons';
+import { DeleteOutlined, EyeTwoTone, PlusCircleOutlined } from '@ant-design/icons';
 import { Space, Table, Button, Form, Tag, Card } from 'antd';
 import { Link, useNavigate } from 'react-router-dom';
-// import CreateAccount from './components/Modal/CreateAccount';
 
-// import 'antd/dist/antd.min.css';
+import { Input, Select } from 'antd';
+const { Option } = Select;
 
 function ManageUser() {
     const [page, setPage] = useState(1);
     const [loadApi, setLoadApi] = useState(false);
+    const [state, setState] = useState({
+        name: '',
+        email: '',
+        phone: '',
+        gender: '',
+        number: '',
+    });
     const dispatch = useDispatch();
     const user = useSelector((state) => state.auth.login?.currentUser);
     const listAccount = useSelector((state) => state.account.account?.accountCurrent);
@@ -70,11 +77,12 @@ function ManageUser() {
             ),
         },
     ];
+
     let dataAccount = {
-        name: '',
-        email: '',
-        phone: '',
-        gender: '',
+        name: state?.name,
+        email: state?.email,
+        phone: state?.phone,
+        gender: state?.gender,
         start: '',
         end: '',
         page: page,
@@ -84,18 +92,27 @@ function ManageUser() {
         member_ship_class_id: '',
         status: '',
     };
+    const handleOnchangeInput = (e) => {
+        let { name, value } = e.target;
+
+        setState({ ...state, [name]: value });
+    };
+
+    const handleSelectChange = (value) => {
+        setState({ ...state, gender: value });
+    };
+
+    useEffect(() => {
+        getAllAccount(dataAccount, dispatch, user?.accessToken);
+        console.log('loadApi useEffect ', loadApi);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [dataAccount?.name || dataAccount?.email || dataAccount?.phone || dataAccount?.gender]);
 
     useEffect(() => {
         getAllAccount(dataAccount, dispatch, user?.accessToken);
         console.log('loadApi useEffect ', loadApi);
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [loadApi || page]);
-    // check biến nào thay đổi thì sẽ load lại hàm bên trong
-
-    // const handleLoadAPI = () => {
-    //     // getAllAccount(dataAccount, dispatch, user?.accessToken);
-    //     setLoadApi(!loadApi);
-    // };
 
     const handleDeleteUser = (account) => {
         console.log('loadApi before ', loadApi);
@@ -109,37 +126,86 @@ function ManageUser() {
         navigate(`/system/manage-user/detail/${account.accounts_id}`);
     };
 
-    // console.log(5 / 4, 'test');
+    const layoutInput = () => {
+        return (
+            <div style={{ display: 'flex' }}>
+                <Input.Group compact>
+                    <Input
+                        style={{ width: '15%', height: 32 }}
+                        placeholder="Search name"
+                        name="name"
+                        value={state?.name}
+                        onChange={handleOnchangeInput}
+                    />
+
+                    <Input
+                        style={{ width: '15%', height: 32 }}
+                        placeholder="Search email"
+                        name="email"
+                        value={state?.email}
+                        onChange={handleOnchangeInput}
+                    />
+                    <Input
+                        style={{ width: '12%', height: 32 }}
+                        placeholder="Search phone"
+                        name="phone"
+                        value={state?.phone}
+                        onChange={handleOnchangeInput}
+                    />
+                    <Select
+                        style={{ width: '12%', height: 32 }}
+                        placeholder="Select gender"
+                        name="gender"
+                        defaultValue="Select"
+                        onChange={handleSelectChange}
+                    >
+                        <Option value="">Tất cả</Option>
+                        <Option value="MALE">Nam</Option>
+                        <Option value="FEMALE">Nữ</Option>
+                    </Select>
+                </Input.Group>
+                {/* <Form.Item label=""> */}
+                <Link to={'/system/manage-user/add'}>
+                    <Button type="primary" style={{ fontWeight: 600, fontSize: 10 }}>
+                        <PlusCircleOutlined />
+                        Add User
+                    </Button>
+                </Link>
+                {/* </Form.Item> */}
+            </div>
+        );
+    };
+
+    console.log('test state', state);
     return (
-        <div style={{ marginTop: '120px' }}>
+        <div style={{ marginTop: '110px' }}>
             <div className="container" style={{ width: '1200px', margin: '0 auto' }}>
                 <Card
                     size="small"
                     title="Total Accounts"
                     // extra={<a href="#">More</a>}
                     style={{
-                        width: 180,
+                        width: 160,
                         height: 140,
+                        marginBottom: 40,
                     }}
                 >
                     <h3 style={{ fontSize: '28px' }}>{listAccount?.totalItems}</h3>
                     <p>Accounts</p>
                 </Card>
-                <Form.Item label="">
-                    {/* <CreateAccount loadApi={handleLoadAPI} accessToken={user?.accessToken} /> */}
+                {/* <Form.Item label="">
                     <Link to={'/system/manage-user/add'}>
                         <Button type="primary" style={{ marginTop: '23px' }}>
                             ADD USER
                         </Button>
                     </Link>
-
-                    {/*<CreateAccount setLoadApi={setLoadApi} loadApi={loadApi} accessToken={user?.accessToken} />*/}
-                </Form.Item>
+                </Form.Item> */}
                 <Table
                     columns={columns}
-                    // { listAccount && listAccount.length > 0 ? dataSource={listAccount} : null}
                     dataSource={listAccount?.content}
                     // rowKey={(listAccount) => listAccount.id}
+                    bordered
+                    title={() => layoutInput()}
                     pagination={{
                         pageSize: 4,
                         total: listAccount?.totalItems,
@@ -148,6 +214,7 @@ function ManageUser() {
                         },
                     }}
                 />
+
                 {/* {listAccount?.totalItems / 5 > 1 && (
                     <Pagination
                         total={listAccount?.totalItems}
