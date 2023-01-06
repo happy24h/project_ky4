bdfc2e9d5b4b21199aab0811599048716e1f498c
@anhnv99 <vietanhhappy99@gmail.com>
2023-01-06 17:29:34 +0700

    update design

--- src/layouts/System/Page/ManageBooking/ManageBooking.js
+++ src/layouts/System/Page/ManageBooking/ManageBooking.js
@@ -1,10 +1,9 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { DatePicker } from 'antd';
 
-import { DeleteOutlined, EyeTwoTone } from '@ant-design/icons';
-import { Space, Table, Button, Form, Card } from 'antd';
-import { deleteBranch, getBranch } from '~/redux/branch/apiBranch';
+import { Button, Form, Card } from 'antd';
 import classNames from 'classnames/bind';
 import styles from './ManageBooking.module.scss';
 import { getBooking } from '~/redux/booking/apiBooking';
@@ -12,14 +11,17 @@ import { getBooking } from '~/redux/booking/apiBooking';
 const cx = classNames.bind(styles);
 
 function ManageBooking() {
-    const [page, setPage] = useState(1);
-    const [loading, setLoading] = useState(false);
     const [state, setState] = useState();
     const dispatch = useDispatch();
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
     const user = useSelector((state) => state.auth.login?.currentUser);
     const listBooking = useSelector((state) => state.booking.booking?.listData);
-    console.log('list booking', listBooking);
+    // console.log('list booking', listBooking);
+
+    const onChange = (date, dateString) => {
+        console.log('test', date, 'aaaa', dateString);
+        setState(dateString);
+    };
 
     let today = new Date();
     if (state) {
@@ -49,29 +51,17 @@ function ManageBooking() {
         getBooking(dataBooking, dispatch, user?.accessToken);
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [loading]);
+    }, []);
     useEffect(() => {
         getBooking(dataBooking, dispatch, user?.accessToken);
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [today]);
-    const handleLoading = () => {
-        setLoading(!loading);
-    };
-
-    // const handleDeleteUser = (data) => {
-    //     // alert('hello world' + data.id);
-    //     deleteBranch(data.id, user?.accessToken, dispatch, handleLoading);
-    // };
-    // const handleEditUser = (data) => {
-    //     // alert('ID: ' + data.id);
-    //     navigate(`/system/manage-branch/detail/${data.id}`);
-    // };
 
     return (
-        <div style={{ marginTop: '120px' }}>
+        <div style={{ marginTop: '106px' }}>
             <div className="container" style={{ width: '1200px', margin: '0 auto' }}>
-                <div className=" l-6 form-group">
+                {/* <div className=" l-6 form-group">
                     <label>Chọn ngày</label>
                     <input
                         type="date"
@@ -81,7 +71,7 @@ function ManageBooking() {
                         required
                         onChange={(e) => setState(e.target.value)}
                     />
-                </div>
+                </div> */}
                 <Card
                     size="small"
                     title="Total Booking"
@@ -89,76 +79,75 @@ function ManageBooking() {
                     style={{
                         width: 160,
                         height: 140,
+                        marginBottom: 25,
                     }}
                 >
                     <h3 style={{ fontSize: '28px' }}>{listBooking?.totalItems}</h3>
                     <p>Booking</p>
                 </Card>
-                <Form.Item label="">
-                    <Link to={'/system/manage-booking/add'}>
-                        <Button type="primary" style={{ marginTop: '23px' }}>
-                            Add booking
-                        </Button>
-                    </Link>
-                </Form.Item>
-                {/* <Table
-                    columns={columns}
-                    dataSource={listBooking?.content?.employee}
-                    pagination={{
-                        pageSize: 3,
-                        total: listBooking?.totalItems,
-                        onChange: (page) => {
-                            setPage(page);
-                        },
+
+                <Card
+                    title={<DatePicker onChange={onChange} />}
+                    extra={
+                        <Form.Item label="">
+                            <Link to={'/system/manage-booking/add'}>
+                                <Button type="primary" style={{ marginTop: '23px', backgroundColor: '#fcaf17' }}>
+                                    Add booking
+                                </Button>
+                            </Link>
+                        </Form.Item>
+                    }
+                    style={{
+                        width: 1200,
+                        minHeight: 600,
+                        backgroundColor: '#e5e5e5',
                     }}
-                /> */}
-                <div className="grid wide">
-                    <div className="row">
-                        {listBooking?.content.map((item, index) => {
-                            // return <div>{item.employee.employee_name}</div>;
-                            return (
-                                <div className="col l-2-4 m-4 c-6">
-                                    <Card
-                                        size="small"
-                                        title="Đặt lịch"
-                                        // extra={<a href="#">More</a>}
-                                        style={{
-                                            // width: 260,
-                                            height: 170,
-                                        }}
-                                        key={index}
-                                    >
-                                        <h3 style={{ fontSize: '20px' }}>{item.employee.employee_name}</h3>
-                                        <p>
-                                            Ngày: <span>{item.employee.bookingByTime_bookings[0].date_booking}</span>
-                                        </p>
-                                        {/* <p>
-                                            Số lượng: <span>{item.employee.bookingByTime_bookings.length}</span>
-                                        </p> */}
-                                        <div style={{ display: 'flex', marginTop: 8 }}>
-                                            {item.employee.bookingByTime_bookings.map((item, index) => {
-                                                return (
-                                                    <div key={index} style={{ marginRight: '5px' }}>
-                                                        <Button
-                                                            type="primary"
-                                                            style={{
-                                                                fontWeight: 600,
-                                                                fontSize: 10,
-                                                                backgroundColor: '#fcaf17',
-                                                            }}
-                                                        >
-                                                            {item.time_booking}h
-                                                        </Button>
-                                                    </div>
-                                                );
-                                            })}
-                                        </div>
-                                    </Card>
-                                </div>
-                            );
-                        })}
+                >
+                    <div className="grid wide">
+                        <div className="row">
+                            {listBooking?.content.map((item, index) => {
+                                return (
+                                    <div className="col l-3 m-4 c-6">
+                                        <Card
+                                            size="small"
+                                            title="Đặt lịch"
+                                            // extra={<a href="#">More</a>}
+                                            style={{
+                                                // width: 260,
+                                                minHeight: 170,
+                                            }}
+                                            key={index}
+                                        >
+                                            <h3 style={{ fontSize: '20px' }}>{item.employee.employee_name}</h3>
+                                            <p>
+                                                Ngày:{' '}
+                                                <span>{item.employee.bookingByTime_bookings[0].date_booking}</span>
+                                            </p>
+                                            <div style={{ display: 'flex', marginTop: 8 }}>
+                                                {item.employee.bookingByTime_bookings.map((item, index) => {
+                                                    return (
+                                                        <div key={index} style={{ marginRight: '5px' }}>
+                                                            <Button
+                                                                type="primary"
+                                                                style={{
+                                                                    fontWeight: 600,
+                                                                    fontSize: 10,
+                                                                    backgroundColor: '#fcaf17',
+                                                                }}
+                                                            >
+                                                                {item.time_booking}h
+                                                            </Button>
+                                                        </div>
+                                                    );
+                                                })}
+                                            </div>
+                                        </Card>
+                                    </div>
+                                );
+                            })}
+                        </div>
                     </div>
-                </div>
+                </Card>
             </div>
         </div>
     );
